f73fc642208dc5e53abb905e08b9c0d7e1c1e3e5
Nikita <nks.off@gmail.com>
2015-11-15 22:11:00 +0300

    task 4, update right block (popular tags, best users)

--- ask_slimov/ask_slimov/context_processors.py
+++ ask_slimov/ask_slimov/context_processors.py
@@ -1,21 +1,14 @@
-from random import choice, shuffle
+from ask_slimov.models import Tag
+from django.contrib.auth.models import User
 
 # popular tags
 def popular_tags(request):
-    tags = ['mysql', 'technopark', 'mail.ru', 'php', 'perl', 'ruby on rails', 'paraboloid', 'binary tree', 'css', 'json', 'cpp', 'binary-tree', 'bootstrap css', 'social network', 'c++11', ]
-    colors = ['success', 'primary', 'default', 'danger', 'info']
+    tags = Tag.objects.order_by_question_count().all()[:12]
 
-    popular = [
-            { 'tag': tag, 'color': choice(colors), } for tag in tags
-    ]
-
-    return {'popular_tags': popular}
+    return {'popular_tags': tags}
 
 # best users
 def best_users(request):
-    users = ['Mr. N', 'D. Medvedev', 'Ivan Smirnov', 'Domodedov Ilya', 'Sheremetyev Michael']
-    shuffle(users)
-    best = [
-        { 'name': u } for u in users
-    ]
-    return {'best_users': best}
+    users = User.objects.all()[0:10] 
+
+    return {'best_users': users}
